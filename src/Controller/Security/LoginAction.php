ef249d217a04b16556dcfa70416a6f641fefb51c
blaschka <jblaschka@umanit.fr>
2024-06-20 21:58:50 +0200

    configuration security + login page + redirection BO

--- src/Controller/Security/LoginAction.php
+++ src/Controller/Security/LoginAction.php
@@ -15,25 +15,10 @@ class LoginAction extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('@EasyAdmin/page/login.html.twig', [
-            'error'                   => $error,
-            'last_username'           => $lastUsername,
-            'translation_domain'      => 'admin',
-            'favicon_path'            => '/favicon-admin.svg',
-            'page_title'              => 'ACME login',
-            'csrf_token_intention'    => 'authenticate',
-            'target_path'             => $this->generateUrl('admin_dashboard'),
-            'username_label'          => 'Your username',
-            'password_label'          => 'Your password',
-            'sign_in_label'           => 'Log in',
-            'username_parameter'      => 'my_custom_username_field',
-            'password_parameter'      => 'my_custom_password_field',
-            'forgot_password_enabled' => true,
-            'forgot_password_label'   => 'Forgot your password?',
-            'remember_me_enabled'     => true,
-            'remember_me_parameter'   => 'custom_remember_me_param',
-            'remember_me_checked'     => true,
-            'remember_me_label'       => 'Remember me',
+        return $this->render('admin/login.html.twig', [
+            'error'         => $error,
+            'last_username' => $lastUsername,
+            'admin_login'   => true,
         ]);
     }
 }
